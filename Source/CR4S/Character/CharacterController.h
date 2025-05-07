2c53a5c26da51baae5134fc95abe8b649347a5f4
2h1824 <hongil0824@naver.com>
2025-05-07 22:50:20 +0900

    [Add] Add Base classes and Inputs
    #1 Add BaseCharacter and CharacterController class
    #2 Add Inputs(IMC, IA)
    #3 Add Move, Jump, Sprint, Look functinos

---
+++ Source/CR4S/Character/CharacterController.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CharacterController.generated.h"
+
+/**
+ * 
+ */
+class UInputMappingContext;
+class UInputAction;
+UCLASS()
+class CR4S_API ACharacterController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	ACharacterController();
+
+#pragma region IMC
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> MoveAction;
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> LookAction;
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> JumpAction;
+	/** Sprint Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> SprintAction;
+#pragma endregion
+};
