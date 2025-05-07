197fc4f56391b7640fdb8b526b308d642447e7ae
KimSeungHwan <ksh951222@gmail.com>
2025-05-07 16:27:53 +0900

    [Add] C4 Core Classes
    1. GameMode, GameState, GameInstace 클래스 원형 추가
    2. PublicIncludePaths에 "CR4S" 경로 추가

---
+++ Source/CR4S/Game/GameMode/C4MenuGameMode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "C4MenuGameMode.generated.h"
+
+UCLASS()
+class CR4S_API AC4MenuGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
