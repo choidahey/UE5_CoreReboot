0e4eb9ac66a06f12a8c7e27982934c04fd2aef3a
leenaaq <v584731@gmail.com>
2025-05-16 01:04:38 +0900

    [Add] Animal AI BTT nodes and behavior tree setup
    1. 동물 AI 상태 구현
    2. 자동화 AI 프로토타입 구현을 통해 구조 설계
    3. 자동화 AI 대기 상태 구현
    4. 자동화 AI 플레이어 RVO 방식 추격 (수정 필요)

---
+++ Source/CR4S/FriendlyAI/Component/FlyingMovementComponent.cpp
@@ -0,0 +1,18 @@
+#include "FlyingMovementComponent.h"
+#include "AIController.h"
+#include "GameFramework/Character.h"
+
+void UFlyingMovementComponent::MoveToLocation(const FVector& Dest)
+{
+
+}
+
+void UFlyingMovementComponent::MoveToActor(AActor* TargetActor)
+{
+
+}
+
+void UFlyingMovementComponent::StopMovement()
+{
+
+}
---
+++ Source/CR4S/FriendlyAI/Component/GroundMovementComponent.cpp
@@ -0,0 +1,36 @@
+#include "GroundMovementComponent.h"
+#include "AIController.h"
+#include "GameFramework/Character.h"
+
+void UGroundMovementComponent::MoveToLocation(const FVector& Dest)
+{
+	if (APawn* OwnerPawn = Cast<APawn>(GetOwner()))
+	{
+		if (AAIController* AICon = Cast<AAIController>(OwnerPawn->GetController()))
+		{
+			AICon->MoveToLocation(Dest);
+		}
+	}
+}
+
+void UGroundMovementComponent::MoveToActor(AActor* TargetActor)
+{
+	if (APawn* OwnerPawn = Cast<APawn>(GetOwner()))
+	{
+		if (AAIController* AICon = Cast<AAIController>(OwnerPawn->GetController()))
+		{
+			AICon->MoveToActor(TargetActor);
+		}
+	}
+}
+
+void UGroundMovementComponent::StopMovement()
+{
+	if (APawn* OwnerPawn = Cast<APawn>(GetOwner()))
+	{
+		if (AAIController* AICon = Cast<AAIController>(OwnerPawn->GetController()))
+		{
+			AICon->StopMovement();
+		}
+	}
+}
