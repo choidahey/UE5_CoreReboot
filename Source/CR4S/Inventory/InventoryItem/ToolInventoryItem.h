0e0af0088595388a4dbdbfe105066ec54295f525
ttingpan <dx123xb@naver.com>
2025-05-26 15:07:13 +0900

    [Add] Tool Inventory Item Data Getter
    - 도구 아이템 데이터 getter 함수 추가

--- Source/CR4S/Inventory/InventoryItem/ToolInventoryItem.h
+++ Source/CR4S/Inventory/InventoryItem/ToolInventoryItem.h
@@ -10,11 +10,21 @@ class CR4S_API UToolInventoryItem : public UBaseInventoryItem
 {
 	GENERATED_BODY()
 
+#pragma region UBaseInventoryItem Override
+	
 public:
 	virtual void UseItem() override;
 
 	virtual void SetInventoryItemData(const FInventoryItemData& NewInventoryItemData, const int32 StackCount = 0) override;
+
+#pragma endregion
+
+#pragma region Data
+public:
+	FORCEINLINE const FToolItemData& GetToolItemData() const { return ToolItemData; }
 	
 private:
 	FToolItemData ToolItemData;
+
+#pragma endregion
 };
