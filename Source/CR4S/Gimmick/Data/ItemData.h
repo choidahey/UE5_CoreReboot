8e85cf7a34d45fbc8bfd0a73e3a0699628177b22
ttingpan <dx123xb@naver.com>
2025-06-04 15:50:41 +0900

    [Add] Crafting Widget / [Update] Item Data Table
    - 제작 위젯 프로토 타입 추가
    - 아이템, 기믹 데이터 테이블 수정
    - 제작법 데이터 테이블 추가

--- Source/CR4S/Gimmick/Data/ItemData.h
+++ Source/CR4S/Gimmick/Data/ItemData.h
@@ -10,23 +10,18 @@ struct FItemInfoData : public FTableRowBase
 {
 	GENERATED_BODY()
 
-	FItemInfoData()
-		: MaxStackCount(64)
-	{
-	}
-
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "이름"))
-	FText Name;
+	FText Name = FText::FromString("");
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "설명"))
-	FText Description;
+	FText Description = FText::FromString("");
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "아이콘"))
-	TObjectPtr<UTexture2D> Icon;
+	TObjectPtr<UTexture2D> Icon = nullptr;
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "태그"))
-	FGameplayTagContainer ItemTags;
+	FGameplayTagContainer ItemTags = FGameplayTagContainer();
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "최대 중첩 개수", ClampMin = "1", ClampMax = "9999"))
-	int32 MaxStackCount;
+	int32 MaxStackCount = 64;
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "디테일 데이터"))
-	FDataTableRowHandle DetailData;
+	FDataTableRowHandle DetailData = FDataTableRowHandle();
 };
 
 USTRUCT(BlueprintType)
@@ -34,20 +29,14 @@ struct FToolItemData : public FTableRowBase
 {
 	GENERATED_BODY()
 
-	FToolItemData()
-		: Damage(0),
-		  StunValue(0)
-	{
-	}
-
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "스태틱 메시"))
-	TObjectPtr<UStaticMesh> StaticMesh;
+	TObjectPtr<UStaticMesh> StaticMesh = nullptr;
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "애니메이션 몽타주"))
-	TObjectPtr<UAnimMontage> AnimMontage;
+	TObjectPtr<UAnimMontage> AnimMontage = nullptr;
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "공격력", ClampMin = "0"))
-	int32 Damage;
+	int32 Damage = 0;
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "기절 수치", ClampMin = "0"))
-	int32 StunValue;
+	int32 StunValue = 0;
 };
 
 USTRUCT(BlueprintType)
@@ -55,39 +44,26 @@ struct FBossMonsterSpawnItemData : public FTableRowBase
 {
 	GENERATED_BODY()
 
-	FBossMonsterSpawnItemData()
-	{
-	}
-
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "보스 몬스터 데이터"))
-	FDataTableRowHandle BossMonsterData;
+	FDataTableRowHandle BossMonsterData = FDataTableRowHandle();
 };
 
 USTRUCT(BlueprintType)
 struct FHarvestedMaterial
 {
 	GENERATED_BODY()
-
-	FHarvestedMaterial()
-		: Count(0)
-	{
-	}
-
+	
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "수확 재료 데이터"))
-	FDataTableRowHandle MaterialData;
+	FDataTableRowHandle MaterialData = FDataTableRowHandle();
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "수확 재료 개수", ClampMin = "0"))
-	int32 Count;
+	int32 Count = 0;
 };
 
 USTRUCT(BlueprintType)
 struct FAnimalItemData : public FTableRowBase
 {
 	GENERATED_BODY()
-
-	FAnimalItemData()
-	{
-	}
-
+	
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "수확 재료 목록"))
 	TArray<FHarvestedMaterial> HarvestedMaterials;
 };
@@ -97,42 +73,28 @@ struct FConsumableItemData : public FTableRowBase
 {
 	GENERATED_BODY()
 
-	FConsumableItemData()
-		: bCanPlant(false),
-		  HungerRestore(0),
-		  HealthRestore(0),
-		  HeatResistanceValue(0),
-		  HeatResistanceDuration(1),
-		  HumidityResistanceValue(0),
-		  HumidityResistanceDuration(1),
-		  ColdResistanceValue(0),
-		  ColdResistanceDuration(1),
-		  ShelfLife(0)
-	{
-	}
-
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "심기 가능 여부"))
-	bool bCanPlant;
+	bool bCanPlant = false;
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "작물 기믹 데이터"))
-	FDataTableRowHandle CropsGimmickData;
+	FDataTableRowHandle CropsGimmickData = FDataTableRowHandle();
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "애니메이션 몽타주"))
-	TObjectPtr<UAnimMontage> AnimMontage;
+	TObjectPtr<UAnimMontage> AnimMontage = nullptr;
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "허기 증감 수치"))
-	int32 HungerRestore;
+	int32 HungerRestore = 0;
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "체력 증감 수치"))
-	int32 HealthRestore;
+	int32 HealthRestore = 0;
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "더위 저항 증감 수치"))
-	int32 HeatResistanceValue;
+	int32 HeatResistanceValue = 0;
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "더위 저항 증감 지속 시간", ToolTip = "단위: 분", ClampMin = "1"))
-	int32 HeatResistanceDuration;
+	int32 HeatResistanceDuration = 1;
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "습도 증감 수치"))
-	int32 HumidityResistanceValue;
+	int32 HumidityResistanceValue = 0;
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "습도 저항 증감 지속 시간", ToolTip = "단위: 분", ClampMin = "1"))
-	int32 HumidityResistanceDuration;
+	int32 HumidityResistanceDuration = 1;
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "추위 저항 증감 수치"))
-	int32 ColdResistanceValue;
+	int32 ColdResistanceValue = 0;
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "추위 저항 증감 지속 시간", ToolTip = "단위: 분", ClampMin = "1"))
-	int32 ColdResistanceDuration;
+	int32 ColdResistanceDuration = 1;
 	UPROPERTY(EditAnywhere, meta = (DisplayName = "보관 시간", ToolTip = "단위: 분", ClampMin = "0"))
-	int32 ShelfLife;
+	int32 ShelfLife = 0;
 };
