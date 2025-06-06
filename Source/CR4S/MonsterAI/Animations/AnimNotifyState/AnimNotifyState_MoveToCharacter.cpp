44ce3685cfc802a31e623aedf9135f738dc05f21
9yutae <9yutae@gmail.com>
2025-06-06 17:42:11 +0900

    [Add] Add AnimNotifyState for moving and rotating monster druing skills
    #01. AnimNotifyState_LookAtTarget : 스킬 사용 도중 몬스터가 플레이어(타겟)으로 회전 및 AimOffset 선택 적용
    #02. AnimNotifyState_MoveToCharacter : 스킬 사용 도중 몬스터가 플레이어 액터로 이동
    #03. AimOffset 적용을 위한 MonsterAnimInstance 추가
    #04. 애니메이션 BP 수정 및 스킬 데이터 수정

---
+++ Source/CR4S/MonsterAI/Animations/AnimNotifyState/AnimNotifyState_MoveToCharacter.cpp
@@ -0,0 +1,108 @@
+#include "MonsterAI/Animations/AnimNotifyState/AnimNotifyState_MoveToCharacter.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Navigation/PathFollowingComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UAnimNotifyState_MoveToCharacter::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration)
+{
+	if (!MeshComp) return;
+
+	CachedCharacter = Cast<ACharacter>(MeshComp->GetOwner());
+	if (!CachedCharacter.IsValid()) return;
+
+	CachedController = Cast<AAIController>(CachedCharacter->GetController());
+	if (!CachedController.IsValid()) return;
+
+	TargetActor = GetTargetActor();
+	ElapsedMoveTime = 0.f;
+
+	if (UCharacterMovementComponent* MoveComp = CachedCharacter->GetCharacterMovement())
+	{
+		MoveComp->MaxWalkSpeed = MoveSpeed;
+	}
+
+	StartMoveTo();
+}
+
+void UAnimNotifyState_MoveToCharacter::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime)
+{
+	ElapsedMoveTime += FrameDeltaTime;
+	if (ElapsedMoveTime >= MoveInterval)
+	{
+		StartMoveTo();
+		ElapsedMoveTime = 0.f;
+	}
+}
+
+void UAnimNotifyState_MoveToCharacter::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	if (!MeshComp) return;
+
+	if (CachedController.IsValid())
+	{
+		CachedController->StopMovement();
+	}
+
+	if (bJumpOnEnd)
+	{
+		LaunchJumpTowardTarget();
+	}
+}
+
+void UAnimNotifyState_MoveToCharacter::StartMoveTo()
+{
+	if (!CachedController.IsValid() || !CachedCharacter.IsValid()) return;
+
+	FAIMoveRequest MoveRequest;
+
+	if (bUseTargetActor && TargetActor)
+	{
+		MoveRequest.SetGoalActor(TargetActor);
+	}
+	else
+	{
+		MoveRequest.SetGoalLocation(MoveToLocation);
+	}
+
+	MoveRequest.SetAcceptanceRadius(AcceptableRadius);
+	MoveRequest.SetUsePathfinding(true);
+	MoveRequest.SetCanStrafe(true);
+	MoveRequest.SetReachTestIncludesAgentRadius(false);
+
+	CachedController->MoveTo(MoveRequest);
+}
+
+void UAnimNotifyState_MoveToCharacter::LaunchJumpTowardTarget()
+{
+	if (!CachedCharacter.IsValid()) return;
+
+	const FVector CurrentLocation = CachedCharacter->GetActorLocation();
+	const FVector TargetLocation = (bUseTargetActor && TargetActor) ? TargetActor->GetActorLocation() : MoveToLocation;
+
+	FVector FlatDir = TargetLocation - CurrentLocation;
+	FlatDir.Z = 0.f;
+
+	if (FlatDir.Size() > MaxJumpDistance)
+	{
+		FlatDir = FlatDir.GetSafeNormal() * MaxJumpDistance;
+	}
+
+	FVector LaunchVelocity = FlatDir;
+	LaunchVelocity.Z = JumpZImpulse;
+
+	CachedCharacter->LaunchCharacter(LaunchVelocity, true, true);
+}
+
+AActor* UAnimNotifyState_MoveToCharacter::GetTargetActor() const
+{
+	if (!CachedController.IsValid()) return nullptr;
+
+	if (UBlackboardComponent* BB = CachedController->GetBlackboardComponent())
+	{
+		return Cast<AActor>(BB->GetValueAsObject(TargetBlackboardKey));
+	}
+	return nullptr;
+}
