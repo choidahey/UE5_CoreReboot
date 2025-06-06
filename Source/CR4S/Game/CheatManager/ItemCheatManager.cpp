ed5e75c1035cc2330a4f621381475e8e24680dbb
ttingpan <dx123xb@naver.com>
2025-06-07 02:59:35 +0900

    [Add] Item Cheat Manager
    - 아이템 치트 매니저 추가
    - 아이템 추가 기능 작동

---
+++ Source/CR4S/Game/CheatManager/ItemCheatManager.cpp
@@ -0,0 +1,24 @@
+﻿#include "ItemCheatManager.h"
+
+#include "Inventory/Components/PlayerInventoryComponent.h"
+
+void UItemCheatManager::AddItem(const FName RowName, const int32 Count) const
+{
+	const APlayerController* PlayerController = GetPlayerController();
+	if (!IsValid(PlayerController))
+	{
+		return;
+	}
+
+	const APawn* Pawn = PlayerController->GetPawn();
+	if (!IsValid(Pawn))
+	{
+		return;
+	}
+
+	UPlayerInventoryComponent* PlayerInventoryComponent = Pawn->FindComponentByClass<UPlayerInventoryComponent>();
+	if (IsValid(PlayerInventoryComponent))
+	{
+		PlayerInventoryComponent->AddItem(RowName, Count);
+	}
+}
---
+++ Source/CR4S/Game/CheatManager/ItemCheatManager.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CheatManager.h"
+#include "ItemCheatManager.generated.h"
+
+UCLASS()
+class CR4S_API UItemCheatManager : public UCheatManager
+{
+	GENERATED_BODY()
+	
+#pragma region AddItem
+
+public:
+	UFUNCTION(Exec)
+	void AddItem(FName RowName, int32 Count) const;
+	
+#pragma endregion 
+};
