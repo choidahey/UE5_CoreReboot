fc28e507e04c31612d3aea6245363c6a3f1870f1
ttingpan <dx123xb@naver.com>
2025-06-10 10:25:12 +0900

    [Add] Resource Pickup Gimmick
    - 주울수 있는 자원 기믹 추가(나무, 돌)
    - 기믹 데이터 테이블에 추가

---
+++ Source/CR4S/Gimmick/GimmickObjects/ResourceGimmick/ResourcePickupGimmick.cpp
@@ -0,0 +1,30 @@
+﻿#include "ResourcePickupGimmick.h"
+
+#include "CR4S.h"
+#include "Gimmick/Components/InteractableComponent.h"
+
+AResourcePickupGimmick::AResourcePickupGimmick()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	InteractableComponent = CreateDefaultSubobject<UInteractableComponent>(TEXT("InteractableComponent"));
+}
+
+void AResourcePickupGimmick::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (CR4S_VALIDATE(LogGimmick, IsValid(InteractableComponent)))
+	{
+		InteractableComponent->OnTryInteract.BindDynamic(this, &ThisClass::OnGimmickInteracted);
+	}
+}
+
+void AResourcePickupGimmick::OnGimmickInteracted(AActor* Interactor)
+{
+	GetResources(Interactor);
+
+	GimmickDestroy();
+}
+
+
