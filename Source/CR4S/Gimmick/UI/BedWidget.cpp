0f147afe82d44f284ba63016e6413f7295d029eb
ttingpan <dx123xb@naver.com>
2025-06-18 15:00:38 +0900

    [Add] Bed Gimmick
    - 침대 기믹 초안 추가

---
+++ Source/CR4S/Gimmick/UI/BedWidget.cpp
@@ -0,0 +1,53 @@
+﻿#include "BedWidget.h"
+
+#include "CR4S.h"
+#include "Components/Button.h"
+#include "UI/InGame/SurvivalHUD.h"
+
+void UBedWidget::InitWidget(APlayerController* PlayerController, const bool bCanSleep)
+{
+	if (IsValid(PlayerController))
+	{
+		SurvivalHUD = Cast<ASurvivalHUD>(PlayerController->GetHUD());
+		if (IsValid(SurvivalHUD))
+		{
+			SurvivalHUD->SetInputMode(ESurvivalInputMode::UIOnly, this, true);
+		}
+	}
+	
+	if (IsValid(SleepButton))
+	{
+		if (bCanSleep)
+		{
+			SleepButton->SetIsEnabled(true);
+			SleepButton->OnClicked.AddUniqueDynamic(this, &ThisClass::HandleSleep);
+		}
+		else
+		{
+			SleepButton->SetIsEnabled(false);
+		}
+	}
+
+	if (IsValid(CancelButton))
+	{
+		CancelButton->OnClicked.AddUniqueDynamic(this, &ThisClass::RemoveWidget);
+	}
+}
+
+void UBedWidget::HandleSleep()
+{
+	CR4S_Log(LogInventoryUI, Warning, TEXT("Sleep!"));
+
+	RemoveWidget();
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void UBedWidget::RemoveWidget()
+{
+	if (IsValid(SurvivalHUD))
+	{
+		SurvivalHUD->SetInputMode(ESurvivalInputMode::GameOnly, nullptr, false);
+	}
+
+	RemoveFromParent();
+}
---
+++ Source/CR4S/Gimmick/UI/BedWidget.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BedWidget.generated.h"
+
+class ASurvivalHUD;
+class UButton;
+
+UCLASS()
+class CR4S_API UBedWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+#pragma region Initialize
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Initialize")
+	void InitWidget(APlayerController* PlayerController, const bool bCanSleep);
+
+private:
+	UPROPERTY()
+	TObjectPtr<ASurvivalHUD> SurvivalHUD;
+	
+#pragma endregion
+
+#pragma region Sleep
+
+public:
+	UFUNCTION()
+	void HandleSleep();
+	
+#pragma endregion
+
+#pragma region ToggleWidget
+
+private:
+	UFUNCTION()
+	void RemoveWidget();
+	
+#pragma endregion 
+	
+#pragma region BindWidget
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> SaveButton;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> SleepButton;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> CancelButton;
+	
+#pragma endregion
+};
