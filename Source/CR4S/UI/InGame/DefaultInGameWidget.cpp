26e0bb045a5344102180b3e14ecee41911e345f4
2h1824 <hongil0824@naver.com>
2025-06-20 02:27:23 +0900

    [Add] Lock On Indicator UI in DefaultInGameWidget
    #1 New widget variable in DefaultInGameWidget
    - UImage : Lock On UI
    #2 Add LockOnImage update functions
    - UpdateImagevisible() : Set LockOnImage Visible(HitTestInvisible) (default : Hidden)
    - UpdateImageInvisible() : Set LockOnImage Hidden
    - SetLockedOnColor() : Change LockOnImage Color to Red
    - UpdateImagePosition() : Change UI position in viewport to NewPosition that indicate target's location in screen
    - BindWidgetToHomingWeapon() : Bind Widget update functions to HomingWeapon's delegate

--- Source/CR4S/UI/InGame/DefaultInGameWidget.cpp
+++ Source/CR4S/UI/InGame/DefaultInGameWidget.cpp
@@ -1,14 +1,24 @@
 #include "DefaultInGameWidget.h"
 
+#include "CR4S.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
 #include "Character/Components/ModularRobotStatusComponent.h"
 #include "Character/Components/PlayerCharacterStatusComponent.h"
 #include "Character/UI/CharacterStatusWidget.h"
+#include "Character/UI/RoundProgressBar.h"
+#include "Character/Weapon/RobotWeapon/HomingWeapon.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Components/Image.h"
 #include "UI/Common/ProgressBarWidget.h"
 #include "UI/InGame/TimeDisplayWidget.h"
 
 void UDefaultInGameWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
+	if (!CR4S_ENSURE(LogHong1,LockOnImage)) return;
+	
+	LockOnImage->SetColorAndOpacity(FLinearColor::White);
+	LockOnImage->SetVisibility(ESlateVisibility::Hidden);
 }
 
 void UDefaultInGameWidget::InitializeStatusWidget(UBaseStatusComponent* InComponent, bool bIsRobot)
@@ -33,6 +43,16 @@ void UDefaultInGameWidget::InitializeStatusWidget(UBaseStatusComponent* InCompon
 		UpdateStunWidget(Percentage);
 	}
 	StatusWidget->ToggleWidgetMode(bIsRobot);
+}
+
+void UDefaultInGameWidget::BindWidgetToHomingWeapon(AHomingWeapon* HomingWeapon)
+{
+	if (!CR4S_ENSURE(LogHong1,HomingWeapon)) return;
+
+	HomingWeapon->OnLockOnStarted.AddDynamic(this,&UDefaultInGameWidget::UpdateImageVisible);
+	HomingWeapon->OnLockOnCanceled.AddDynamic(this,&UDefaultInGameWidget::UpdateImageInvisible);
+	HomingWeapon->OnLockOnFinished.AddDynamic(this,&UDefaultInGameWidget::SetLockedOnColor);
+	HomingWeapon->OnTryingToLockOn.AddDynamic(this,&UDefaultInGameWidget::UpdateImagePosition);
 }
 
 void UDefaultInGameWidget::UpdateHPWidget(const float InPercentage)
@@ -84,4 +104,39 @@ void UDefaultInGameWidget::UpdateTimeWidget(FWorldTimeData CurrentTimeData)
 		TimeDisplayWidget->UpdateTime(CurrentTimeData.Day, CurrentTimeData.Minute, CurrentTimeData.Second);
 	}
 }
+
+void UDefaultInGameWidget::UpdateImageVisible()
+{
+	if (!CR4S_ENSURE(LogHong1,LockOnImage)) return;
+	LockOnImage->SetColorAndOpacity(FLinearColor::White);
+	LockOnImage->SetVisibility(ESlateVisibility::HitTestInvisible);
+}
+
+void UDefaultInGameWidget::UpdateImageInvisible()
+{
+	if (!CR4S_ENSURE(LogHong1,LockOnImage)) return;
+	LockOnImage->SetVisibility(ESlateVisibility::Hidden);
+}
+
+void UDefaultInGameWidget::SetLockedOnColor()
+{
+	if (!CR4S_ENSURE(LogHong1,LockOnImage)) return;
+	LockOnImage->SetColorAndOpacity(FLinearColor::Red);
+}
+
+void UDefaultInGameWidget::UpdateImagePosition(const FVector2D& NewPosition)
+{
+	if (!CR4S_ENSURE(LogHong1,LockOnImage)) return;
+
+	if (UCanvasPanelSlot* ImageSlot=UWidgetLayoutLibrary::SlotAsCanvasSlot(LockOnImage))
+	{
+		const float ViewportScale=UWidgetLayoutLibrary::GetViewportScale(this);
+		FVector2D LocalPosition=NewPosition/ViewportScale;
+
+		ImageSlot->SetAlignment({0.5f,0.5f});
+		ImageSlot->SetPosition(LocalPosition);
+		
+	}
+}
+
 
