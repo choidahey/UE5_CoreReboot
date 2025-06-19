1915772451ad932555230e2f80974efab70323dc
leenaaq <v584731@gmail.com>
2025-06-19 11:22:46 +0900

    [Fix] Resolve Bird service instance sharing issue

--- Source/CR4S/FriendlyAI/Tasks/FA/BTTask_FASetRandomMove.h
+++ Source/CR4S/FriendlyAI/Tasks/FA/BTTask_FASetRandomMove.h
@@ -4,6 +4,12 @@
 #include "BehaviorTree/BTTaskNode.h"
 #include "BTTask_FASetRandomMove.generated.h"
 
+struct FBTTask_FASetRandomMoveMemory
+{
+	FRotator TargetRotation;
+	float RotationInterpSpeed = 2.f;
+};
+
 UCLASS()
 class CR4S_API UBTTask_FASetRandomMove : public UBTTaskNode
 {
@@ -15,8 +21,5 @@ public:
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 	virtual void OnGameplayTaskActivated(UGameplayTask& Task) override {}
-
-protected:
-	FRotator TargetRotation;
-	float RotationInterpSpeed = 2.f;
+	virtual uint16 GetInstanceMemorySize() const override;
 };
