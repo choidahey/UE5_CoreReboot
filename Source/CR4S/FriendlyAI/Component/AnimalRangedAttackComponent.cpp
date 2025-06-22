84794cd2ecc723726acbb87936f257653efda52b
leenaaq <v584731@gmail.com>
2025-06-23 03:56:25 +0900

    [Fix] Animal Ranged Attack
    발사 컴포넌트 로직 수정
    (목표물에 맞게 발사)

--- Source/CR4S/FriendlyAI/Component/AnimalRangedAttackComponent.cpp
+++ Source/CR4S/FriendlyAI/Component/AnimalRangedAttackComponent.cpp
@@ -1,11 +1,14 @@
 #include "AnimalRangedAttackComponent.h"
 
+#include "AIController.h"
 #include "ObjectPoolComponent.h"
 #include "Game/System/ProjectilePoolSubsystem.h"
 #include "FriendlyAI/Projectile/AnimalProjectile.h"
 #include "GameFramework/Actor.h"
 #include "Components/ArrowComponent.h"
 #include "../BaseAnimal.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 UAnimalRangedAttackComponent::UAnimalRangedAttackComponent()
 {
@@ -44,8 +47,31 @@ void UAnimalRangedAttackComponent::FireProjectile()
 		PoolComp->ReturnToPoolAfter(ProjectileLifetime);
 	}
 
+	AAIController* AICon = Cast<AAIController>(OwnerAnimal->GetController());
+	UBlackboardComponent* BB = AICon ? AICon->FindComponentByClass<UBlackboardComponent>() : nullptr;
+	AActor* Target = BB ? Cast<AActor>(BB->GetValueAsObject("TargetActor")) : nullptr;
+
+	FVector Direction;
+	if (Target)
+	{
+		FVector SpawnLoc = SpawnTransform.GetLocation();
+		FRotator MuzzleRot = SpawnTransform.GetRotation().Rotator();
+		FVector ToTarget = Target->GetActorLocation() - SpawnLoc;
+		FRotator DesiredRot = ToTarget.Rotation();
+		FRotator DeltaRot = UKismetMathLibrary::NormalizedDeltaRotator(DesiredRot, MuzzleRot);
+
+		float ClampedYaw   = FMath::Clamp(DeltaRot.Yaw,   -MaxYawAngle,   MaxYawAngle);
+		float ClampedPitch = FMath::Clamp(DeltaRot.Pitch, -MaxPitchAngle, MaxPitchAngle);
+
+		Direction = (MuzzleRot + FRotator(ClampedPitch, ClampedYaw, 0)).Vector();
+	}
+	else
+	{
+		Direction = SpawnTransform.GetRotation().GetForwardVector();
+	}
+
 	if (Projectile->ProjectileMovement)
 	{
-		Projectile->ProjectileMovement->Velocity = SpawnTransform.GetRotation().GetForwardVector() * ProjectileSpeed;
+		Projectile->ProjectileMovement->Velocity = Direction * ProjectileSpeed;
 	}
 }
