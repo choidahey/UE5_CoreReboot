12ca934519fbb4f271d34c54bec2a6ea9302cd75
KimSeungHwan <ksh951222@gmail.com>
2025-06-28 16:01:21 +0900

    [Add] Graphics Settings Feature
    1.윈도우 모드, 해상도, 품질 설정 기능 추가

--- Source/CR4S/UI/MainMenu/SettingsWidget.cpp
+++ Source/CR4S/UI/MainMenu/SettingsWidget.cpp
@@ -3,6 +3,7 @@
 #include "UI/MainMenu/MainMenuWidget.h"
 #include "UI/Common/NotificationWidget.h"
 #include "Components/Button.h"
+#include "Components/TextBlock.h"
 #include "Components/Border.h"
 #include "Components/VerticalBox.h"
 #include "Components/Slider.h"
@@ -11,7 +12,8 @@
 #include "Game/SaveGame/SaveGameManager.h"
 #include "Game/SaveGame/SettingsSaveGame.h"
 #include "Kismet/GameplayStatics.h"
-
+#include "GameFramework/GameUserSettings.h"
+#include "Engine/Engine.h"
 
 void USettingsWidget::NativeConstruct()
 {
@@ -27,6 +29,8 @@ void USettingsWidget::NativeConstruct()
 		WindowWidget->OnApplyClicked.AddDynamic(this, &USettingsWidget::RequestSaveSettings);
 	}
 
+
+
 	if (IsCategoryValid())
 	{
 		GameplayButton->OnClicked.AddDynamic(this, &USettingsWidget::HandleGamePlayButtonClicked);
@@ -34,6 +38,51 @@ void USettingsWidget::NativeConstruct()
 		GraphicsButton->OnClicked.AddDynamic(this, &USettingsWidget::HandleGraphicsButtonClicked);
 		ControlsButton->OnClicked.AddDynamic(this, &USettingsWidget::HandleControlsButtonClicked);
 	}
+
+	if (WindowModeLeftButton)
+	{
+		WindowModeLeftButton->OnClicked.AddDynamic(this, &USettingsWidget::OnWindowModeLeftClicked);
+	}
+	if (WindowModeRightButton)
+	{
+		WindowModeRightButton->OnClicked.AddDynamic(this, &USettingsWidget::OnWindowModeRightClicked);
+	}
+	if (ResolutionLeftButton)
+	{
+		ResolutionLeftButton->OnClicked.AddDynamic(this, &USettingsWidget::OnResolutionLeftClicked);
+	}
+	if (ResolutionRightButton)
+	{
+		ResolutionRightButton->OnClicked.AddDynamic(this, &USettingsWidget::OnResolutionRightClicked);
+	}
+	if (QualityLeftButton)
+	{
+		QualityLeftButton->OnClicked.AddDynamic(this, &USettingsWidget::OnGraphicsQualityLeftClicked);
+	}
+	if (QualityRightButton)
+	{
+		QualityRightButton->OnClicked.AddDynamic(this, &USettingsWidget::OnGraphicsQualityRightClicked);
+	}
+
+	ResolutionOptions = {
+	FIntPoint(1280, 720),
+	FIntPoint(1600, 900),
+	FIntPoint(1920, 1080),
+	FIntPoint(2560, 1440),
+	FIntPoint(3840, 2160)
+	};
+
+	FIntPoint CurrentResolution = GEngine->GetGameUserSettings()->GetScreenResolution();
+	for (int32 i = 0; i < ResolutionOptions.Num(); ++i)
+	{
+		if (ResolutionOptions[i] == CurrentResolution)
+		{
+			CurrentResolutionIndex = i;
+			break;
+		}
+	}
+
+	UpdateResolutionText();
 
 	MasterVolumeSlider->OnValueChanged.AddDynamic(this, &USettingsWidget::OnMasterVolumeChanged);
 	BGMVolumeSlider->OnValueChanged.AddDynamic(this, &USettingsWidget::OnBGMVolumeChanged);
@@ -172,6 +221,153 @@ bool USettingsWidget::IsCategoryValid()
 		&& ControlsSettings && ControlsButton;
 }
 
+#pragma region "Graphics Settings"
+void USettingsWidget::OnWindowModeLeftClicked()
+{
+	CurrentWindowModeIndex = (CurrentWindowModeIndex - 1 + WindowModeOptions.Num()) % WindowModeOptions.Num();
+	UpdateWindowModeText();
+	ApplyWindowModeSetting();
+}
+
+void USettingsWidget::OnWindowModeRightClicked()
+{
+	CurrentWindowModeIndex = (CurrentWindowModeIndex + 1) % WindowModeOptions.Num();
+	UpdateWindowModeText();
+	ApplyWindowModeSetting();
+}
+
+void USettingsWidget::UpdateWindowModeText()
+{
+	if (!WindowModeText) return;
+
+	FString ModeString;
+	switch (WindowModeOptions[CurrentWindowModeIndex])
+	{
+	case EWindowModeOption::Fullscreen:
+		ModeString = TEXT("Full Screen");
+		break;
+	case EWindowModeOption::WindowedFullscreen:
+		ModeString = TEXT("Windowed Fullscreen");
+		break;
+	case EWindowModeOption::Windowed:
+		ModeString = TEXT("Windowed");
+		break;
+	}
+
+	WindowModeText->SetText(FText::FromString(ModeString));
+}
+
+void USettingsWidget::ApplyWindowModeSetting()
+{
+	EWindowMode::Type NewMode;
+	switch (WindowModeOptions[CurrentWindowModeIndex])
+	{
+	case EWindowModeOption::Fullscreen:
+		NewMode = EWindowMode::Fullscreen;
+		break;
+	case EWindowModeOption::WindowedFullscreen:
+		NewMode = EWindowMode::WindowedFullscreen;
+		break;
+	case EWindowModeOption::Windowed:
+		NewMode = EWindowMode::Windowed;
+		break;
+	default:
+		NewMode = EWindowMode::Windowed;
+		break;
+	}
+
+	if (GEngine && GEngine->GetGameUserSettings())
+	{
+		UGameUserSettings* Settings = GEngine->GetGameUserSettings();
+		Settings->SetFullscreenMode(NewMode);
+		Settings->ApplySettings(false);
+	}
+}
+
+void USettingsWidget::OnResolutionLeftClicked()
+{
+	CurrentResolutionIndex = (CurrentResolutionIndex - 1 + ResolutionOptions.Num()) % ResolutionOptions.Num();
+	UpdateResolutionText();
+	ApplyResolutionSetting();
+}
+
+void USettingsWidget::OnResolutionRightClicked()
+{
+	CurrentResolutionIndex = (CurrentResolutionIndex + 1) % ResolutionOptions.Num();
+	UpdateResolutionText();
+	ApplyResolutionSetting();
+}
+
+void USettingsWidget::UpdateResolutionText()
+{
+	if (!ResolutionText) return;
+
+	FIntPoint Res = ResolutionOptions[CurrentResolutionIndex];
+	FString ResStr = FString::Printf(TEXT("%d X %d"), Res.X, Res.Y);
+	ResolutionText->SetText(FText::FromString(ResStr));
+}
+
+void USettingsWidget::ApplyResolutionSetting()
+{
+	if (GEngine && GEngine->GetGameUserSettings())
+	{
+		UGameUserSettings* Settings = GEngine->GetGameUserSettings();
+		Settings->SetScreenResolution(ResolutionOptions[CurrentResolutionIndex]);
+		Settings->ApplySettings(false);
+	}
+}
+
+void USettingsWidget::OnGraphicsQualityLeftClicked()
+{
+	CurrentGraphicsQualityIndex = (CurrentGraphicsQualityIndex - 1 + GraphicsQualityOptions.Num()) % GraphicsQualityOptions.Num();
+	UpdateGraphicsQualityText();
+	ApplyGraphicsQualitySetting();
+}
+
+void USettingsWidget::OnGraphicsQualityRightClicked()
+{
+	CurrentGraphicsQualityIndex = (CurrentGraphicsQualityIndex + 1) % GraphicsQualityOptions.Num();
+	UpdateGraphicsQualityText();
+	ApplyGraphicsQualitySetting();
+}
+
+void USettingsWidget::UpdateGraphicsQualityText()
+{
+	if (!QualityText) return;
+
+	FString QualityStr;
+	switch (GraphicsQualityOptions[CurrentGraphicsQualityIndex])
+	{
+	case EGraphicsQualityOption::Epic: QualityStr = TEXT("Epic"); break;
+	case EGraphicsQualityOption::High: QualityStr = TEXT("High"); break;
+	case EGraphicsQualityOption::Medium: QualityStr = TEXT("Medium"); break;
+	case EGraphicsQualityOption::Low: QualityStr = TEXT("Low"); break;
+	}
+	QualityText->SetText(FText::FromString(QualityStr));
+}
+
+void USettingsWidget::ApplyGraphicsQualitySetting()
+{
+	if (!GEngine || !GEngine->GetGameUserSettings()) return;
+
+	UGameUserSettings* Settings = GEngine->GetGameUserSettings();
+
+	int32 QualityLevel = 3; // Epic
+	switch (GraphicsQualityOptions[CurrentGraphicsQualityIndex])
+	{
+	case EGraphicsQualityOption::Epic: QualityLevel = 3; break;
+	case EGraphicsQualityOption::High: QualityLevel = 2; break;
+	case EGraphicsQualityOption::Medium: QualityLevel = 1; break;
+	case EGraphicsQualityOption::Low: QualityLevel = 0; break;
+	}
+
+	// 전체 항목 품질 적용
+	Settings->SetOverallScalabilityLevel(QualityLevel);
+	Settings->ApplySettings(false);
+}
+
+#pragma endregion
+
 #pragma region "Audio Settings"
 
 void USettingsWidget::OnMasterVolumeChanged(float Value)
@@ -217,6 +413,13 @@ void USettingsWidget::RequestSaveSettings()
 			}
 		}
 	}
+
+	if (GEngine && GEngine->GetGameUserSettings())
+	{
+		UGameUserSettings* Settings = GEngine->GetGameUserSettings();
+		Settings->SaveSettings();
+	}
+	// index 저장
 }
 
 void USettingsWidget::LoadSettingsData()
