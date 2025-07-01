1c87fbd04dc927b26a6157553204315070c81667
2h1824 <hongil0824@naver.com>
2025-07-01 13:05:59 +0900

    [Add] Add new tool assets (Axe,Pickaxe,Hammer)

---
+++ Source/CR4S/Game/CheatManager/CharacterCheatHelper.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "CharacterCheatHelper.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CR4S_API UCharacterCheatHelper : public UObject
+{
+	GENERATED_BODY()
+};
